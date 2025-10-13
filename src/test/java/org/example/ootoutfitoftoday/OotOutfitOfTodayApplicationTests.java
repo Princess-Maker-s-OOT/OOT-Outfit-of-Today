c114198d6770c7ac4d41a38b28097f78f47aa18f
cjn404 <moi404@naver.com>
2025-10-13 15:47:47 +0900

    first commit

---
+++ src/test/java/org/example/ootoutfitoftoday/OotOutfitOfTodayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ootoutfitoftoday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OotOutfitOfTodayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
