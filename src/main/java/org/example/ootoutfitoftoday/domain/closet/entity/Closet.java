bbeeefe962ebf9f419a5e1c83ba9968c6449a757
zerone <bazz0324@naver.com>
2025-10-16 16:24:30 +0900

    [init] closet: Closet 엔티티 초기 세팅
    - 'ClosetClothesLink' 추후 연관관계 매핑 예정
    - @Bilder패턴 및 정적팩토리메서드 적용

--- src/main/java/org/example/ootoutfitoftoday/domain/closet/entity/Closet.java
+++ src/main/java/org/example/ootoutfitoftoday/domain/closet/entity/Closet.java
@@ -1,4 +1,59 @@
 package org.example.ootoutfitoftoday.domain.closet.entity;
 
-public class Closet {
+import jakarta.persistence.*;
+import lombok.*;
+import org.example.ootoutfitoftoday.common.entity.BaseEntity;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "closets")
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class Closet extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String name;
+
+    @Column(length = 255, nullable = true)
+    private String description;
+
+    @Column(length = 500, nullable = true)
+    private String imageUrl;
+
+    @Column(nullable = false)
+    private Boolean visibility = false;
+
+//    @OneToMany(mappedBy = "closet")
+//    private List<ClosetClothesLink> closetClothesLinks = new ArrayList<>();
+
+    @Builder
+    private Closet(
+            String name,
+            String description,
+            String imageUrl,
+            Boolean visibility
+    ) {
+        this.name = name;
+        this.description = description;
+        this.imageUrl = imageUrl;
+        this.visibility = visibility;
+    }
+
+    public static Closet create(
+            String name,
+            String description,
+            String imageUrl,
+            Boolean visibility
+    ) {
+        return Closet.builder()
+                .name(name)
+                .description(description)
+                .imageUrl(imageUrl)
+                .visibility(visibility)
+                .build();
+    }
 }
