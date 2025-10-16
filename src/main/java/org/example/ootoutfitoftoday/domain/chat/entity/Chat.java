ebc0f70f3abfd06b20e9d6a1cbe1aebb2dc77604
Sungho <nanumeran@gmail.com>
2025-10-16 17:22:43 +0900

    [init] chat : 초기 세팅

--- src/main/java/org/example/ootoutfitoftoday/domain/chat/entity/Chat.java
+++ src/main/java/org/example/ootoutfitoftoday/domain/chat/entity/Chat.java
@@ -1,4 +1,32 @@
 package org.example.ootoutfitoftoday.domain.chat.entity;
 
-public class Chat {
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.ootoutfitoftoday.common.entity.BaseEntity;
+
+@Entity
+@Getter
+@Table(name = "chats")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Chat extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    @Builder(access = AccessLevel.PROTECTED)
+    private Chat(String content) {
+        this.content = content;
+    }
+
+    public Chat create(String content) {
+        return Chat.builder()
+                .content(content)
+                .build();
+    }
 }
--- src/main/java/org/example/ootoutfitoftoday/domain/chat/repository/ChatRepository.java
+++ src/main/java/org/example/ootoutfitoftoday/domain/chat/repository/ChatRepository.java
@@ -1,4 +1,7 @@
 package org.example.ootoutfitoftoday.domain.chat.repository;
 
-public interface ChatRepository {
+import org.example.ootoutfitoftoday.domain.chat.entity.Chat;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChatRepository extends JpaRepository<Chat, Long> {
 }
