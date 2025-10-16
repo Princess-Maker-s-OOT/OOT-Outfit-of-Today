3050bb886042a44a0a153262f898fbd8bbb63dc1
zerone <bazz0324@naver.com>
2025-10-16 21:38:07 +0900

    [init] closetclotheslink: ClosetClothesLink 엔티티 연관관계 매핑
    - Clothes ↔ ClosetClothesLink ↔ Closet 연관관계 매핑

--- src/main/java/org/example/ootoutfitoftoday/domain/closetclotheslink/entity/ClosetClothesLink.java
+++ src/main/java/org/example/ootoutfitoftoday/domain/closetclotheslink/entity/ClosetClothesLink.java
@@ -1,4 +1,27 @@
 package org.example.ootoutfitoftoday.domain.closetclotheslink.entity;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.ootoutfitoftoday.domain.closet.entity.Closet;
+import org.example.ootoutfitoftoday.domain.clothes.entity.Clothes;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "closet_clothes_links")
 public class ClosetClothesLink {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "closet_id", nullable = false)
+    private Closet closet;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "clothes_id", nullable = false)
+    private Clothes clothes;
 }
