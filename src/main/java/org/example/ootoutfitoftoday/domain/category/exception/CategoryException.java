d38d232a42bf7cdf46b41c0d72385b3b9746d6f6
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-10-18 10:52:23 +0900

    [init] clothes: 옷 성공코드, 에러코드 초기 세팅

--- src/main/java/org/example/ootoutfitoftoday/domain/category/exception/CategoryException.java
+++ src/main/java/org/example/ootoutfitoftoday/domain/category/exception/CategoryException.java
@@ -3,6 +3,7 @@ package org.example.ootoutfitoftoday.domain.category.exception;
 import org.example.ootoutfitoftoday.common.exception.GlobalException;
 
 public class CategoryException extends GlobalException {
+
     public CategoryException(CategoryErrorCode categoryErrorCode) {
         super(categoryErrorCode);
     }
