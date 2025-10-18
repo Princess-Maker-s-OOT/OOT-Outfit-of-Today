f3a3ef3a9ea590995c7577a2d2ff4a4164a0cce3
cjn404 <moi404@naver.com>
2025-10-19 00:06:26 +0900

    [feat] auth: AuthSuccessCode 구현

---
+++ src/main/java/org/example/ootoutfitoftoday/domain/auth/exception/AuthSuccessCode.java
@@ -0,0 +1,17 @@
+package org.example.ootoutfitoftoday.domain.auth.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.example.ootoutfitoftoday.common.exception.SuccessCode;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum AuthSuccessCode implements SuccessCode {
+
+    USER_SIGNUP("USER_SIGNUP", HttpStatus.CREATED, "회원가입이 완료되었습니다.");
+
+    private final String code;
+    private final HttpStatus httpStatus;
+    private final String message;
+}
