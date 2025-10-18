bce4997dfabd4518f3fe8ae14d439ee343f60e7c
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-10-18 16:52:47 +0900

    [feat] clothes: 옷 엔티티와 디티오 구현
    - 유저 값 주석처리 (유저는 추후에 연관관계 맺어서 구현할 예정)

--- src/main/java/org/example/ootoutfitoftoday/domain/clothes/dto/request/ClothesRequest.java
+++ src/main/java/org/example/ootoutfitoftoday/domain/clothes/dto/request/ClothesRequest.java
@@ -1,4 +1,19 @@
 package org.example.ootoutfitoftoday.domain.clothes.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import org.example.ootoutfitoftoday.domain.clothes.enums.ClothesColor;
+import org.example.ootoutfitoftoday.domain.clothes.enums.ClothesSize;
+
+@Getter
 public class ClothesRequest {
+
+    private Long categoryId;
+    private ClothesSize clothesSize;
+    private ClothesColor clothesColor;
+
+    @NotBlank
+    @Size(min = 1, max = 255, message = "255자 이하로 입력해 주세요!")
+    private String description;
 }
--- src/main/java/org/example/ootoutfitoftoday/domain/clothes/dto/response/ClothesResponse.java
+++ src/main/java/org/example/ootoutfitoftoday/domain/clothes/dto/response/ClothesResponse.java
@@ -1,4 +1,37 @@
 package org.example.ootoutfitoftoday.domain.clothes.dto.response;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.example.ootoutfitoftoday.domain.clothes.entity.Clothes;
+import org.example.ootoutfitoftoday.domain.clothes.enums.ClothesColor;
+import org.example.ootoutfitoftoday.domain.clothes.enums.ClothesSize;
+
+@Getter
+@Builder
+@RequiredArgsConstructor
 public class ClothesResponse {
+
+    private final Long id;
+    private final Long categoryId;
+//    private final User user;
+    private final ClothesSize clothesSize;
+    private final ClothesColor clothesColor;
+    private final String description;
+
+    public static ClothesResponse from(Clothes clothes) {
+
+        return ClothesResponse.builder()
+                .id(clothes.getId())
+                .categoryId(
+                        clothes.getCategory() != null
+                                ? clothes.getCategory().getId()
+                                : null
+                )
+//                .user(clothes.getUser())
+                .clothesSize(clothes.getClothesSize())
+                .clothesColor(clothes.getClothesColor())
+                .description(clothes.getDescription())
+                .build();
+    }
 }
