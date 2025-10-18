caa74d49ba9a4c485f1a92a448e3186c800325ce
zerone <bazz0324@naver.com>
2025-10-18 16:55:28 +0900

    [feat] closet: ClosetErrorCode 생성
    - Closet 도메인의 에러 응답 코드 정의
    - ErrorCode 인터페이스 구현

--- src/main/java/org/example/ootoutfitoftoday/domain/closet/exception/ClosetErrorCode.java
+++ src/main/java/org/example/ootoutfitoftoday/domain/closet/exception/ClosetErrorCode.java
@@ -1,4 +1,17 @@
 package org.example.ootoutfitoftoday.domain.closet.exception;
 
-public enum ClosetErrorCode {
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.example.ootoutfitoftoday.common.exception.ErrorCode;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ClosetErrorCode implements ErrorCode {
+
+    CLOSET_UNAUTHORIZED("CLOSET_UNAUTHORIZED", HttpStatus.FORBIDDEN, "옷장에 접근할 권한이 없습니다.");
+
+    private final String code;
+    private final HttpStatus httpStatus;
+    private final String message;
 }
