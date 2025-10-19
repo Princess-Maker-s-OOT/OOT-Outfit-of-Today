95632c5b9f27cb0188a23c69d1acefae7253425a
cjn404 <moi404@naver.com>
2025-10-19 15:08:05 +0900

    [fix] auth: 메서드별 접근 권한 구분

--- src/main/java/org/example/ootoutfitoftoday/security/config/SecurityConfig.java
+++ src/main/java/org/example/ootoutfitoftoday/security/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.example.ootoutfitoftoday.domain.user.enums.UserRole;
 import org.example.ootoutfitoftoday.security.filter.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -41,15 +42,14 @@ public class SecurityConfig {
 
                 // JWT 사용 시 불필요한 기능들 비활성화
                 .formLogin(AbstractHttpConfigurer::disable)      // [SSR] 서버가 로그인 HTML 폼 렌더링
-                .anonymous(AbstractHttpConfigurer::disable)      // 미인증 사용자를 익명으로 처리
+                //.anonymous(AbstractHttpConfigurer::disable)      // 미인증 사용자를 익명으로 처리
                 .httpBasic(AbstractHttpConfigurer::disable)      // [SSR] 인증 팝업
                 .logout(AbstractHttpConfigurer::disable)         // [SSR] 서버가 세션 무효화 후 리다이렉트
                 .rememberMe(AbstractHttpConfigurer::disable)     // 서버가 쿠키 발급하여 자동 로그인
 
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(
-                                "/auth/v1/signup",
-                                "/auth/v1/login",
+                        .requestMatchers(HttpMethod.POST, "/auth/v1/signup", "/auth/v1/login").permitAll()
+                        .requestMatchers(HttpMethod.GET,
                                 "/api/v1/closets/public",
                                 "/api/v1/sale-posts",
                                 "/api/v1/sale-posts/{salePostId}",
