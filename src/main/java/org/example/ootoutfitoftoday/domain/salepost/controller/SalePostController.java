c108649d7140782cfb1105970020a56bc8ad8abb
assokk <mijisuu@naver.com>
2025-10-20 07:08:37 +0900

    [feat] SalePost: 판매글 상세 조회 Controller 구현
    - GET /v1/sale-posts/{salePostId}
    - SUCCESSCODE 추가
      - SALE_POST_RETRIEVED

--- src/main/java/org/example/ootoutfitoftoday/domain/salepost/controller/SalePostController.java
+++ src/main/java/org/example/ootoutfitoftoday/domain/salepost/controller/SalePostController.java
@@ -6,14 +6,13 @@ import org.example.ootoutfitoftoday.common.response.ApiResponse;
 import org.example.ootoutfitoftoday.domain.auth.dto.AuthUser;
 import org.example.ootoutfitoftoday.domain.salepost.dto.request.SalePostCreateRequest;
 import org.example.ootoutfitoftoday.domain.salepost.dto.response.SalePostCreateResponse;
+import org.example.ootoutfitoftoday.domain.salepost.dto.response.SalePostDetailResponse;
 import org.example.ootoutfitoftoday.domain.salepost.exception.SalePostSuccessCode;
 import org.example.ootoutfitoftoday.domain.salepost.service.command.SalePostCommandService;
+import org.example.ootoutfitoftoday.domain.salepost.service.query.SalePostQueryService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/sale-posts")
@@ -21,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class SalePostController {
 
     private final SalePostCommandService salePostCommandService;
+    private final SalePostQueryService salePostQueryService;
 
     @PostMapping
     public ResponseEntity<ApiResponse<SalePostCreateResponse>> createSalePost(
@@ -34,5 +34,12 @@ public class SalePostController {
         );
 
         return ApiResponse.success(response, SalePostSuccessCode.SALE_POSTS_CREATED);
+    }
+
+    @GetMapping("/{salePostId}")
+    public ResponseEntity<ApiResponse<SalePostDetailResponse>> getSalePostDetail(@PathVariable Long salePostId) {
+        SalePostDetailResponse response = salePostQueryService.getSalePostDetail(salePostId);
+
+        return ApiResponse.success(response, SalePostSuccessCode.SALE_POST_RETRIEVED);
     }
 }
