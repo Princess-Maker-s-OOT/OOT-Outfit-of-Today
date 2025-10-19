fb4ad36b3ee5437329e0fee22e2d4d08bcdfc5c7
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-10-19 23:35:41 +0900

    [refactor] user: 유저 에러 코드 및 성공 코드 구현

--- src/main/java/org/example/ootoutfitoftoday/domain/user/exception/UserSuccessCode.java
+++ src/main/java/org/example/ootoutfitoftoday/domain/user/exception/UserSuccessCode.java
@@ -2,14 +2,14 @@ package org.example.ootoutfitoftoday.domain.user.exception;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.example.ootoutfitoftoday.common.exception.ErrorCode;
+import org.example.ootoutfitoftoday.common.exception.SuccessCode;
 import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor
-public enum UserErrorCode implements ErrorCode {
+public enum UserSuccessCode implements SuccessCode {
 
-    USER_NOT_FOUND("USER_NOT_FOUND", HttpStatus.NOT_FOUND, "유저를 찾을 수 없습니다!");
+    USER_SUCCESS_CODE("USER_SUCCESS_CODE",HttpStatus.OK, "임시 성공 코드입니다!"),;
 
     private final String code;
     private final HttpStatus httpStatus;
