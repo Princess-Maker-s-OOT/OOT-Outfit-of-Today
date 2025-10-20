4a38c4baa50f3a93153619c79524c3bed84a5c89
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-10-20 15:39:00 +0900

    [feat] category: 카테고리 조회 성공 API 구현
    - 페이지네이션 처리
    - 기본 값은 생성일 기준으로 내림차순 정렬

--- src/main/java/org/example/ootoutfitoftoday/domain/category/service/query/CategoryQueryService.java
+++ src/main/java/org/example/ootoutfitoftoday/domain/category/service/query/CategoryQueryService.java
@@ -1,10 +1,19 @@
 package org.example.ootoutfitoftoday.domain.category.service.query;
 
+import org.example.ootoutfitoftoday.domain.category.dto.response.CategoryResponse;
 import org.example.ootoutfitoftoday.domain.category.entity.Category;
+import org.springframework.data.domain.Page;
 
 import java.util.Optional;
 
 public interface CategoryQueryService {
 
     Optional<Category> findById(long id);
+
+    Page<CategoryResponse> getCategories(
+            int page,
+            int size,
+            String sort,
+            String direction
+    );
 }
--- src/main/java/org/example/ootoutfitoftoday/domain/category/service/query/CategoryQueryServiceImpl.java
+++ src/main/java/org/example/ootoutfitoftoday/domain/category/service/query/CategoryQueryServiceImpl.java
@@ -1,8 +1,13 @@
 package org.example.ootoutfitoftoday.domain.category.service.query;
 
 import lombok.RequiredArgsConstructor;
+import org.example.ootoutfitoftoday.domain.category.dto.response.CategoryResponse;
 import org.example.ootoutfitoftoday.domain.category.entity.Category;
 import org.example.ootoutfitoftoday.domain.category.repository.CategoryRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,5 +24,27 @@ public class CategoryQueryServiceImpl implements CategoryQueryService {
     public Optional<Category> findById(long id) {
 
         return categoryRepository.findById(id);
+    }
+
+    @Override
+    public Page<CategoryResponse> getCategories(
+            int page,
+            int size,
+            String sort,
+            String direction
+    ) {
+
+        Pageable pageable = PageRequest.of(
+                page,
+                size,
+                Sort.by(
+                        Sort.Direction.fromString(direction), // asc or desc
+                        sort // 정렬 기준 필드명
+                )
+        );
+
+        Page<Category> categories = categoryRepository.findAllByIsDeletedFalse(pageable);
+
+        return categories.map(CategoryResponse::from);
     }
 }
