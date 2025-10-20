10199823c08e3cd2e744ad06a22b7340047547df
zerone <bazz0324@naver.com>
2025-10-20 14:29:16 +0900

    [feat] closet: ClosetSuccessCode에 삭제 성공 코드 추가
    - CLOSET_DELETE_OK 성공 코드 추가 (200 OK)

--- src/main/java/org/example/ootoutfitoftoday/domain/closet/exception/ClosetSuccessCode.java
+++ src/main/java/org/example/ootoutfitoftoday/domain/closet/exception/ClosetSuccessCode.java
@@ -13,7 +13,8 @@ public enum ClosetSuccessCode implements SuccessCode {
     CLOSETS_GET_PUBLIC_OK("CLOSETS_GET_PUBLIC_OK", HttpStatus.OK, "공개 옷장 리스트를 조회했습니다."),
     CLOSET_GET_OK("CLOSET_GET_OK", HttpStatus.OK, "옷장 상세 정보를 조회했습니다."),
     CLOSETS_GET_MY_OK("CLOSETS_GET_MY_OK", HttpStatus.OK, "내 옷장 리스트를 조회했습니다."),
-    CLOSET_UPDATE_OK("CLOSET_UPDATE_OK", HttpStatus.OK, "옷장 정보가 수정되었습니다.");
+    CLOSET_UPDATE_OK("CLOSET_UPDATE_OK", HttpStatus.OK, "옷장 정보가 수정되었습니다."),
+    CLOSET_DELETE_OK("CLOSET_DELETE_OK", HttpStatus.OK, "옷장이 삭제되었습니다.");
 
     private final String code;
     private final HttpStatus httpStatus;
