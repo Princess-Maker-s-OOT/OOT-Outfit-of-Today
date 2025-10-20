dab44695d616bb340ac88e05a9337ad8d50b3955
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-10-20 21:01:07 +0900

    [feat] category: 카테고리 삭제 성공 코드 추가

--- src/main/java/org/example/ootoutfitoftoday/domain/category/exception/CategorySuccessCode.java
+++ src/main/java/org/example/ootoutfitoftoday/domain/category/exception/CategorySuccessCode.java
@@ -11,7 +11,8 @@ public enum CategorySuccessCode implements SuccessCode {
 
     CATEGORY_CREATED("CATEGORY_CREATED", HttpStatus.CREATED, "카테고리를 생성 하였습니다!"),
     CATEGORY_OK("CATEGORY_OK", HttpStatus.OK, "카테고리를 조회 하였습니다!"),
-    CATEGORY_UPDATE("CATEGORY_UPDATE", HttpStatus.OK, "카테고리를 수정하였습니다!");
+    CATEGORY_UPDATE("CATEGORY_UPDATE", HttpStatus.OK, "카테고리를 수정하였습니다!"),
+    CATEGORY_DELETE("CATEGORY_DELETE", HttpStatus.OK, "카테고리를 삭제하였습니다!");
 
     private final String code;
     private final HttpStatus httpStatus;
