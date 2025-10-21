c1fda4b04ff0ba6362e5257138a8d6d31187edda
cjn404 <moi404@naver.com>
2025-10-21 14:24:01 +0900

    [feat] auth: 회원가입 시 띄어쓰기 관련 비허용 정규식 추가
    - 닉네임: 앞뒤 공백 불가
    - 이름: 띄어쓰기 불가

--- src/main/java/org/example/ootoutfitoftoday/domain/auth/dto/request/AuthSignupRequest.java
+++ src/main/java/org/example/ootoutfitoftoday/domain/auth/dto/request/AuthSignupRequest.java
@@ -11,14 +11,18 @@ public class AuthSignupRequest {
 
     private static final String ID_REGEX = "^[a-zA-Z0-9_]+$";
 
-    private static final String EMAIL_LOCAL_PART = "^(?!\\.)[A-Za-z0-9._%+-]+(?<!\\.)";
+    private static final String EMAIL_LOCAL_PART = "(?!\\.)[A-Za-z0-9._%+-]+(?<!\\.)";
     private static final String EMAIL_DOMAIN = "@[A-Za-z0-9](?:[A-Za-z0-9-]*[A-Za-z0-9])?(?:\\.[A-Za-z0-9](?:[A-Za-z0-9-]*[A-Za-z0-9])?)*";
-    private static final String EMAIL_TLD = "\\.[A-Za-z]{2,}$";
-    private static final String EMAIL_REGEX = EMAIL_LOCAL_PART + EMAIL_DOMAIN + EMAIL_TLD;
+    private static final String EMAIL_TLD = "\\.[A-Za-z]{2,}";
+    private static final String EMAIL_REGEX = "^" + EMAIL_LOCAL_PART + EMAIL_DOMAIN + EMAIL_TLD + "$";
 
-    private static final String PASSWORD_REGEX = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*(),.?\":{}|<>]).{8,30}$";
+    private static final String PASSWORD_REGEX = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*(),.?\":{}|<>])[^\\s]+$";
 
     private static final String PHONE_NUMBER_REGEX = "^01[016789]\\d{7,8}$";
+
+    private static final String NICKNAME_REGEX = "^(?!\\s).*(?<!\\s)$";
+
+    private static final String USERNAME_REGEX = "^\\S+$";
 
     @NotBlank(message = "아이디는 필수 입력값입니다.")
     @Size(min = 4, max = 15, message = "아이디는 4~15자 사이여야 합니다.")
@@ -33,17 +37,18 @@ public class AuthSignupRequest {
 
     @NotBlank(message = "닉네임은 필수 입력값입니다.")
     @Size(min = 2, max = 10, message = "닉네임은 2~10자 사이여야 합니다.")
+    @Pattern(regexp = NICKNAME_REGEX, message = "닉네임 전후 공백은 불가합니다.")
     private String nickname;
 
     @NotBlank(message = "이름은 필수 입력값입니다.")
     @Size(min = 2, max = 50, message = "이름은 2~50자 사이여야 합니다.")
+    @Pattern(regexp = USERNAME_REGEX, message = "이름 내 공백은 불가합니다.")
     private String username;
 
     @NotBlank(message = "비밀번호는 필수 입력값입니다.")
     @Size(min = 8, max = 30, message = "비밀번호는 8~30자 사이여야 합니다.")
     @Pattern(regexp = PASSWORD_REGEX,
-            message = "비밀번호는 최소 8자, 최대 30자이며, " +
-                    "대소문자 불문 영문, 숫자, 특수문자를 모두 포함해야 합니다.")
+            message = "비밀번호는 대소문자 불문 영문, 숫자, 특수문자를 모두 포함해야 하고 공백은 불가합니다.")
     private String password;
 
     @NotBlank(message = "전화번호는 필수 입력값입니다.")
