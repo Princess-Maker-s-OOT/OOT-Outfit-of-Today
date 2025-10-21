a2ee6063a77fa0451fbc8b3a1be2a715a7553953
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-10-21 12:02:53 +0900

    [feat] category: 카테고리 레포지토리 JPQL 구현
    - 카테고리를 소프트 딜리트 처리할 때 사용할 연관 카테고리들 조회하는 쿼리 구현
    - 조회된 카테고리 아이디들의 값을 업데이트하는 쿼리 구현 -> 소프트 딜리트 처리

--- src/main/java/org/example/ootoutfitoftoday/domain/category/repository/CategoryRepository.java
+++ src/main/java/org/example/ootoutfitoftoday/domain/category/repository/CategoryRepository.java
@@ -4,7 +4,11 @@ import org.example.ootoutfitoftoday.domain.category.entity.Category;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
@@ -15,4 +19,21 @@ public interface CategoryRepository extends JpaRepository<Category, Long> {
     Page<Category> findAllByIsDeletedFalse(Pageable pageable);
 
     Optional<Category> findByIdAndIsDeletedFalse(Long id);
+
+    @Query("""
+            SELECT c.id
+            FROM Category c
+            WHERE c.parent.id IN :parentIds
+              AND c.isDeleted = false
+            """)
+    List<Long> findIdsByParentIds(@Param("parentIds") List<Long> parentIds);
+
+    @Modifying
+    @Query("""
+                UPDATE Category c
+                SET c.isDeleted = true,
+                    c.deletedAt = CURRENT_TIMESTAMP
+                WHERE c.id IN :categoryIds
+            """)
+    void softDeleteCategories(@Param("categoryIds") List<Long> categoryIds);
 }
