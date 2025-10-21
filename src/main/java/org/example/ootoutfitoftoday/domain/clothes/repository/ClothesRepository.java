c9545c1fec067971f021e1ab76fd504568ecc4ee
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-10-21 11:57:01 +0900

    [feat] clothes: 옷 레포지토리 JPQL 구현
    - 카테고리가 소프트 딜리트 처리될 때 연관된 옷의 카테고리 아이디 값을 null 처리하는 JPQL 구현

--- src/main/java/org/example/ootoutfitoftoday/domain/clothes/repository/ClothesRepository.java
+++ src/main/java/org/example/ootoutfitoftoday/domain/clothes/repository/ClothesRepository.java
@@ -2,10 +2,23 @@ package org.example.ootoutfitoftoday.domain.clothes.repository;
 
 import org.example.ootoutfitoftoday.domain.clothes.entity.Clothes;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ClothesRepository extends JpaRepository<Clothes, Long>, CustomClothesRepository {
 
     Optional<Clothes> findByIdAndIsDeletedFalse(Long id);
+
+    // 카테고리가 소프트 딜리트 처리될 때 연관된 옷의 카테고리 아이디 값을 null 처리
+    @Modifying
+    @Query("""
+            UPDATE Clothes c
+            SET c.category = NULL
+            WHERE c.category.id IN :categoryIds
+            """)
+    void clearCategoryFromClothes(@Param("categoryIds") List<Long> categoryIds);
 }
