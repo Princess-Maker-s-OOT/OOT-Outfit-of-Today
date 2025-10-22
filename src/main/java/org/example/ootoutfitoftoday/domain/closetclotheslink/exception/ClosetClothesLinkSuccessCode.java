e249a66e64953ffeae9b2a1c59453b4cce60bac6
zerone <bazz0324@naver.com>
2025-10-22 09:59:05 +0900

    [feat] closetclotheslink: ClosetClothesLinkSuccessCode에 삭제 성공 코드 추가
    - ClosetClothesLinkSuccessCode에 CLOSET_CLOTHES_DELETED 200 OK 응답 추가

--- src/main/java/org/example/ootoutfitoftoday/domain/closetclotheslink/exception/ClosetClothesLinkSuccessCode.java
+++ src/main/java/org/example/ootoutfitoftoday/domain/closetclotheslink/exception/ClosetClothesLinkSuccessCode.java
@@ -10,7 +10,8 @@ import org.springframework.http.HttpStatus;
 public enum ClosetClothesLinkSuccessCode implements SuccessCode {
 
     CLOSET_CLOTHES_LINKED("CLOSET_CLOTHES_LINKED", HttpStatus.CREATED, "옷장에 옷이 등록되었습니다."),
-    CLOSET_CLOTHES_LIST_OK("CLOSET_CLOTHES_LIST_OK", HttpStatus.OK, "옷장에 등록된 옷 리스트를 조회했습니다.");
+    CLOSET_CLOTHES_LIST_OK("CLOSET_CLOTHES_LIST_OK", HttpStatus.OK, "옷장에 등록된 옷 리스트를 조회했습니다."),
+    CLOSET_CLOTHES_DELETED("CLOSET_CLOTHES_DELETED", HttpStatus.OK, "옷장에서 옷이 제거되었습니다.");
 
     private final String code;
     private final HttpStatus httpStatus;
