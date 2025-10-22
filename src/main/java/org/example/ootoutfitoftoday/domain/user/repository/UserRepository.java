493b6bae24252695dd1837674f56f1e35b31f402
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-10-22 15:36:51 +0900

    [feat] user: 대시보드용 유저 JPQL 구현
    - 전체 유저 수 JPQL 구현
    - 활성 or 비활성 유저 수 JPQL 구현
    - 신규 가입자 수 (기간별 집계) JPQL 구현

--- src/main/java/org/example/ootoutfitoftoday/domain/user/repository/UserRepository.java
+++ src/main/java/org/example/ootoutfitoftoday/domain/user/repository/UserRepository.java
@@ -2,7 +2,9 @@ package org.example.ootoutfitoftoday.domain.user.repository;
 
 import org.example.ootoutfitoftoday.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -18,4 +20,31 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByLoginIdAndIsDeletedFalse(String loginId);
 
     Optional<User> findByIdAndIsDeletedFalse(Long id);
+
+    // 전체 유저 수
+    @Query("""
+            SELECT count(u)
+            FROM User u
+            WHERE u.role = 'ROLE_USER'
+            """)
+    int countAllUsers();
+
+    // 활성 or 비활성 유저 수
+    @Query("""
+            SELECT count(u)
+            FROM User u
+            WHERE u.role = 'ROLE_USER'
+              AND u.isDeleted = :isDeleted
+            """)
+    int countByIsDeleted(Boolean isDeleted);
+
+    // 신규 가입자 수 (기간별 집계)
+    @Query("""
+            SELECT count(u)
+            FROM User u
+            WHERE u.role = 'ROLE_USER'
+              AND u.createdAt >= :start
+              AND u.createdAt < :end
+            """)
+    int countUsersRegisteredSince(LocalDateTime start, LocalDateTime end);
 }
