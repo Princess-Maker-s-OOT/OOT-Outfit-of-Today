9f9c872c63c92bee6068b64bcf96ffabb0e099b9
zerone <bazz0324@naver.com>
2025-10-22 09:58:01 +0900

    [feat] closetclotheslink: 연결 해제 관련 에러 코드 추가
    - ClosetClothesLinkErrorCode에 CLOTHES_NOT_LINKED 400 BAD_REQUEST 응답 추가
    - 연결되지 않은 옷 제거 시도 시 사용

--- src/main/java/org/example/ootoutfitoftoday/domain/closetclotheslink/exception/ClosetClothesLinkErrorCode.java
+++ src/main/java/org/example/ootoutfitoftoday/domain/closetclotheslink/exception/ClosetClothesLinkErrorCode.java
@@ -10,7 +10,8 @@ import org.springframework.http.HttpStatus;
 public enum ClosetClothesLinkErrorCode implements ErrorCode {
 
     CLOSET_CLOTHES_ALREADY_LINKED("CLOTHES_ALREADY_LINKED", HttpStatus.BAD_REQUEST, "이미 옷장에 등록된 옷입니다."),
-    CLOSET_CLOTHES_FORBIDDEN("CLOSET_CLOTHES_FORBIDDEN", HttpStatus.FORBIDDEN, "해당 옷장에 대한 권한이 없습니다.");
+    CLOSET_CLOTHES_FORBIDDEN("CLOSET_CLOTHES_FORBIDDEN", HttpStatus.FORBIDDEN, "해당 옷장에 대한 권한이 없습니다."),
+    CLOTHES_NOT_LINKED("CLOTHES_NOT_LINKED", HttpStatus.NOT_FOUND, "옷장에 등록되지 않은 옷입니다.");
 
     private final String code;
     private final HttpStatus httpStatus;
