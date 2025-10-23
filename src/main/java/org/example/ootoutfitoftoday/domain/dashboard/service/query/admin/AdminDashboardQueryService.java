1be43081117aa5b9acc57e5c4c1595ed2a5cca38
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-10-23 22:19:29 +0900

    [feat] dashboard: 등록된 판매글 수 조회 + 거래현황 조회 API 구현
    - 전체적으로 변수명 리팩토링
    - 반환 값
    - 전체 판매글 수
    - 상태별 판매글 수
    - 새로 생성된 판매글 수 (기간별 집계)
    - 파람 값으로 특정 기간을 기입하지 않는다면 api 실행 기준 시각으로 기간별 집계 구현

--- src/main/java/org/example/ootoutfitoftoday/domain/dashboard/service/query/admin/AdminDashboardQueryService.java
+++ src/main/java/org/example/ootoutfitoftoday/domain/dashboard/service/query/admin/AdminDashboardQueryService.java
@@ -1,6 +1,7 @@
 package org.example.ootoutfitoftoday.domain.dashboard.service.query.admin;
 
 import org.example.ootoutfitoftoday.domain.dashboard.dto.response.AdminClothesStatisticsResponse;
+import org.example.ootoutfitoftoday.domain.dashboard.dto.response.AdminSalePostStatisticsResponse;
 import org.example.ootoutfitoftoday.domain.dashboard.dto.response.AdminUserStatisticsResponse;
 
 import java.time.LocalDate;
@@ -10,4 +11,6 @@ public interface AdminDashboardQueryService {
     AdminUserStatisticsResponse adminUserStatistics(LocalDate baseDate);
 
     AdminClothesStatisticsResponse adminClothesStatistics();
+
+    AdminSalePostStatisticsResponse adminSalePostStatistics(LocalDate baseDate);
 }
