13a85d9cda3ac1cf9edba522dd4a92290f034c4a
cjn404 <moi404@naver.com>
2025-10-24 23:38:25 +0900

    [feat] user: @Tag 어노테이션 추가

--- src/main/java/org/example/ootoutfitoftoday/domain/user/controller/UserController.java
+++ src/main/java/org/example/ootoutfitoftoday/domain/user/controller/UserController.java
@@ -3,6 +3,7 @@ package org.example.ootoutfitoftoday.domain.user.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.example.ootoutfitoftoday.common.response.Response;
@@ -17,6 +18,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "회원 관리", description = "회원 관련 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1/user/me")
