5781dafb7bf88db5ac83c854c424a913d88498b4
Sungho <nanumeran@gmail.com>
2025-10-25 00:53:05 +0900

    [feat] salePost : 판매글 리스트 조회 방식 변경
    - CreateRequestDto 필드 추가
    - UpdateRequestDto 필드 추가
    - NotNull 어노테이션 설정
    - DecimalMin,DecimalMax 어노테이션 카카오맵 기준 위도 경도 범위 설정

--- src/main/java/org/example/ootoutfitoftoday/domain/salepost/dto/request/SalePostUpdateRequest.java
+++ src/main/java/org/example/ootoutfitoftoday/domain/salepost/dto/request/SalePostUpdateRequest.java
@@ -25,6 +25,19 @@ public class SalePostUpdateRequest {
     @NotNull(message = "카테고리 선택은 필수입니다.")
     private Long categoryId;
 
+    @NotNull(message = "주소는 필수입니다.")
+    private String tradeAddress;
+
+    @NotNull(message = "위도는 필수입니다.")
+    @DecimalMin(value = "33.0", message = "위도는 33 이상이어야 합니다.")
+    @DecimalMax(value = "38.6", message = "위도는 38.6 이하이어야 합니다.")
+    private BigDecimal tradeLatitude;   // 위도
+
+    @NotNull(message = "경도는 필수입니다.")
+    @DecimalMin(value = "124.6", message = "경도는 124.6 이상이어야 합니다.")
+    @DecimalMax(value = "131.9", message = "경도는 131.9 이하이어야 합니다.")
+    private BigDecimal tradeLongitude;  // 경도
+
     @NotEmpty(message = "이미지는 최소 1개 이상 필요합니다.")
     private List<String> imageUrls;
 }
