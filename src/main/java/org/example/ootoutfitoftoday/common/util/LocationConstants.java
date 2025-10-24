3c2ff4441a0be8a17be7184ee3768ece33ed7841
Sungho <nanumeran@gmail.com>
2025-10-24 12:42:07 +0900

    [feat] user : 유저 거래 위치 수정 API
    - 상수 클래스 생성

---
+++ src/main/java/org/example/ootoutfitoftoday/common/util/LocationConstants.java
@@ -0,0 +1,7 @@
+package org.example.ootoutfitoftoday.common.util;
+
+public final class LocationConstants {
+
+    public static final String DEFAULT_TRADE_ADDRESS = "서울특별시 중구 세종대로 110";
+    public static final String DEFAULT_TRADE_LOCATION = "POINT(37.56681294 126.97865509)";
+}
