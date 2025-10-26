23d200081d6b0c4f633e1f2ebfc2b7384b7e4b58
zerone <bazz0324@naver.com>
2025-10-27 08:38:04 +0900

    [feat] image: ImageSuccessCode 코드 추가
    - IMAGE_SAVED 성공코드 추가

--- src/main/java/org/example/ootoutfitoftoday/domain/image/exception/ImageSuccessCode.java
+++ src/main/java/org/example/ootoutfitoftoday/domain/image/exception/ImageSuccessCode.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ImageSuccessCode implements SuccessCode {
 
-    PRESIGNED_URL_CREATED("PRESIGNED_URL_CREATED", HttpStatus.OK, "Presigned URL이 생성되었습니다.");
+    PRESIGNED_URL_CREATED("PRESIGNED_URL_CREATED", HttpStatus.OK, "Presigned URL이 생성되었습니다."),
+    IMAGE_SAVED("IMAGE_SAVED", HttpStatus.CREATED, "이미지가 저장되었습니다.");
 
     private final String code;
     private final HttpStatus httpStatus;
