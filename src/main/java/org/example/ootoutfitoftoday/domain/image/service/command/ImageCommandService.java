68cef48787cfb64b8fd6a91bd34fde8acd4db0ae
zerone <bazz0324@naver.com>
2025-10-27 08:39:09 +0900

    [feat] image: ImageCommandService saveImage 메서드 추가
    - 이미지 메타데이터 저장 메서드 정의

--- src/main/java/org/example/ootoutfitoftoday/domain/image/service/command/ImageCommandService.java
+++ src/main/java/org/example/ootoutfitoftoday/domain/image/service/command/ImageCommandService.java
@@ -1,6 +1,8 @@
 package org.example.ootoutfitoftoday.domain.image.service.command;
 
+import org.example.ootoutfitoftoday.domain.image.dto.request.ImageSaveRequest;
 import org.example.ootoutfitoftoday.domain.image.dto.request.PresignedUrlRequest;
+import org.example.ootoutfitoftoday.domain.image.dto.response.ImageSaveResponse;
 import org.example.ootoutfitoftoday.domain.image.dto.response.PresignedUrlResponse;
 
 public interface ImageCommandService {
@@ -10,4 +12,7 @@ public interface ImageCommandService {
             Long userId,
             PresignedUrlRequest request
     );
+
+    // 이미지 메타데이터 저장
+    ImageSaveResponse saveImage(ImageSaveRequest request);
 }
