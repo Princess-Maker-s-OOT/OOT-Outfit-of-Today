9ad7473d3ba369f10f3c084d5c6a5a4870caa29b
cjn404 <moi404@naver.com>
2025-10-26 19:46:20 +0900

    [fix] auth: 빈 중복 등록으로 인한 오류 발생 -> 어노테이션 삭제

--- src/main/java/org/example/ootoutfitoftoday/common/config/SchedulerConfig.java
+++ src/main/java/org/example/ootoutfitoftoday/common/config/SchedulerConfig.java
@@ -1,9 +1,7 @@
 package org.example.ootoutfitoftoday.common.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
-@EnableJpaAuditing
 public class SchedulerConfig {
 }
