76189eb3fd49f137696248ee297c2e99efb178bf
zerone <bazz0324@naver.com>
2025-10-26 20:20:42 +0900

    [feat] image: ImageType enum 생성
    - S3 폴더 구조를 위한 이미지 타입 정의
    - CLOSET, CLOTHES, SALEPOST, USER
    - fromString() 메서드로 문자열 변환

--- src/main/java/org/example/ootoutfitoftoday/domain/image/entity/ImageType.java
+++ src/main/java/org/example/ootoutfitoftoday/domain/image/entity/ImageType.java
@@ -1,8 +1,32 @@
 package org.example.ootoutfitoftoday.domain.image.entity;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum ImageType {
-    CLOSET,
-    CLOTHES,
-    SALEPOST,
-    USER
+    CLOSET("closets", "옷장 이미지"),
+    CLOTHES("clothes", "옷 이미지"),
+    SALEPOST("saleposts", "판매글 이미지"),
+    USER("users", "회원정보 이미지");
+
+    private final String folder;  // S3 폴더명
+    private final String description;
+
+    /**
+     * 문자열을 ImageType으로 변환
+     *
+     * @param type 이미지 타입 문자열
+     * @return ImageType
+     * @throws IllegalArgumentException 유효하지 않은 타입인 경우
+     */
+    public static ImageType fromString(String type) {
+        try {
+
+            return ImageType.valueOf(type.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("유효하지 않은 이미지 타입입니다: " + type);
+        }
+    }
 }
