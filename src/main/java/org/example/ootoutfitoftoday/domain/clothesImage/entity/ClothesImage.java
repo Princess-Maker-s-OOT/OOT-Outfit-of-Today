3dc999e23450c0474bc0e40b2757735b004dcbc6
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-10-30 15:03:49 +0900

    [init] ClothesImage: 옷-이미지 중간테이블 엔티티 및 레포 생성

---
+++ src/main/java/org/example/ootoutfitoftoday/domain/clothesImage/entity/ClothesImage.java
@@ -0,0 +1,56 @@
+package org.example.ootoutfitoftoday.domain.clothesImage.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.ootoutfitoftoday.common.entity.BaseEntity;
+import org.example.ootoutfitoftoday.domain.clothes.entity.Clothes;
+import org.example.ootoutfitoftoday.domain.image.entity.Image;
+
+@Entity
+@Getter
+@Table(name = "clothes_images")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ClothesImage extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 옷 연관관계
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "clothes_id", nullable = false)
+    private Clothes clothes;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "image_id", nullable = false)
+    private Image image;
+
+    private Boolean isMain;
+
+    @Builder(access = AccessLevel.PRIVATE)
+    private ClothesImage(
+            Clothes clothes,
+            Image image,
+            Boolean isMain
+    ) {
+        this.clothes = clothes;
+        this.image = image;
+        this.isMain = isMain;
+    }
+
+    public static ClothesImage create(
+            Clothes clothes,
+            Image image,
+            Boolean isMain
+    ) {
+
+        return ClothesImage.builder()
+                .clothes(clothes)
+                .image(image)
+                .isMain(isMain)
+                .build();
+    }
+}
---
+++ src/main/java/org/example/ootoutfitoftoday/domain/clothesImage/repository/ClothesImageRepository.java
@@ -0,0 +1,7 @@
+package org.example.ootoutfitoftoday.domain.clothesImage.repository;
+
+import org.example.ootoutfitoftoday.domain.clothesImage.entity.ClothesImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClothesImageRepository extends JpaRepository<ClothesImage, Long> {
+}
