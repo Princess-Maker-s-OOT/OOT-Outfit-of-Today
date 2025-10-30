161925bd53f07d3c19e923c09e45ed898c244328
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-10-30 23:57:31 +0900

    [feat] clothes: 옷 수정 API + 이미지 수정 포함 구현
    - ClothesImageCommandService에 이미지 수정에 대한 책임을 분리
    - updateMain 메서드를 활용해 새로운 이미지의 대한 메인이미지 여부도 수정하도록 구현

--- src/main/java/org/example/ootoutfitoftoday/domain/clothesImage/repository/ClothesImageRepository.java
+++ src/main/java/org/example/ootoutfitoftoday/domain/clothesImage/repository/ClothesImageRepository.java
@@ -2,6 +2,17 @@ package org.example.ootoutfitoftoday.domain.clothesImage.repository;
 
 import org.example.ootoutfitoftoday.domain.clothesImage.entity.ClothesImage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ClothesImageRepository extends JpaRepository<ClothesImage, Long> {
+
+    @Query("""
+            SELECT ci
+            FROM ClothesImage ci
+            JOIN FETCH ci.image
+            WHERE ci.clothes.id = :clothesId and ci.isDeleted = false
+            """)
+    List<ClothesImage> findByClothesId(Long clothesId);
 }
--- src/main/java/org/example/ootoutfitoftoday/domain/clothesImage/service/command/ClothesImageCommandService.java
+++ src/main/java/org/example/ootoutfitoftoday/domain/clothesImage/service/command/ClothesImageCommandService.java
@@ -7,4 +7,6 @@ import java.util.List;
 public interface ClothesImageCommandService {
 
     void saveClothesImages(Clothes clothes, List<Long> imageIds);
+
+    void updateClothesImages(Clothes clothes, List<Long> newImageIds);
 }
