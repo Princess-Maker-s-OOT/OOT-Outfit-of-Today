a2f09260570325ed4512a677028299ea4b26a59d
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-10-30 15:15:28 +0900

    [feat] image: 옷 등록 API에 사용할 이미지 조회 쿼리

--- src/main/java/org/example/ootoutfitoftoday/domain/image/service/query/ImageQueryService.java
+++ src/main/java/org/example/ootoutfitoftoday/domain/image/service/query/ImageQueryService.java
@@ -2,7 +2,11 @@ package org.example.ootoutfitoftoday.domain.image.service.query;
 
 import org.example.ootoutfitoftoday.domain.image.entity.Image;
 
+import java.util.List;
+
 public interface ImageQueryService {
 
     Image findImageById(Long imageId);
+
+    List<Image> findAllByIdIn(List<Long> imageIds);
 }
