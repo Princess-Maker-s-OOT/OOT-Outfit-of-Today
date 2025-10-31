c4ca54be71dc3546c462113d3db32573ce0e689f
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-10-31 17:24:41 +0900

    [feat] ClothesImage: 에러코드 구현
    - 중복 방지 예외 처리

---
+++ src/main/java/org/example/ootoutfitoftoday/domain/clothesImage/exception/ClothesImageErrorCode.java
@@ -0,0 +1,17 @@
+package org.example.ootoutfitoftoday.domain.clothesImage.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.example.ootoutfitoftoday.common.exception.ErrorCode;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ClothesImageErrorCode implements ErrorCode {
+
+    IMAGE_ALREADY_LINKED("IMAGE_ALREADY_LINKED", HttpStatus.CONFLICT, "이미 다른 옷과 연결된 이미지입니다.");
+
+    private final String code;
+    private final HttpStatus httpStatus;
+    private final String message;
+}
