3399bc2f1758dbc52378941f1807569ecc50da54
cjn404 <moi404@naver.com>
2025-11-01 02:41:17 +0900

    [feat] user: 프로필 이미지 관련 성공 응답 코드 추가

--- src/main/java/org/example/ootoutfitoftoday/domain/user/exception/UserSuccessCode.java
+++ src/main/java/org/example/ootoutfitoftoday/domain/user/exception/UserSuccessCode.java
@@ -11,6 +11,7 @@ public enum UserSuccessCode implements SuccessCode {
 
     GET_MY_INFO("GET_MY_INFO", HttpStatus.OK, "회원정보 조회 완료입니다."),
     UPDATE_MY_INFO("UPDATE_MY_INFO", HttpStatus.OK, "회원정보 수정 완료입니다."),
+    UPDATE_MY_PROFILE_IMAGE("UPDATE_MY_PROFILE_IMAGE", HttpStatus.OK, "프로필 이미지 수정 완료입니다."),
     PASSWORD_VERIFIED("PASSWORD_VERIFIED", HttpStatus.OK, "인증이 완료되었습니다."),
     UPDATED_TRADE_LOCATION("UPDATED_TRADE_LOCATION", HttpStatus.OK, "거래 위치가 변경되었습니다.");
 
