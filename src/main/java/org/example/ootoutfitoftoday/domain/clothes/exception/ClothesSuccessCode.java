1a98b5e7e6c10ed17dc42448cde62700b244a65f
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-10-31 20:50:46 +0900

    [feat] Clothes: 옷-이미지 연관관계 제거 성공 코드 구현

--- src/main/java/org/example/ootoutfitoftoday/domain/clothes/exception/ClothesSuccessCode.java
+++ src/main/java/org/example/ootoutfitoftoday/domain/clothes/exception/ClothesSuccessCode.java
@@ -12,7 +12,8 @@ public enum ClothesSuccessCode implements SuccessCode {
     CLOTHES_CREATED("CLOTHES_CREATED", HttpStatus.CREATED, "옷을 등록하였습니다!"),
     CLOTHES_OK("CLOTHES_OK",HttpStatus.OK, "옷을 성공적으로 조회하였습니다!"),
     CLOTHES_UPDATE("CLOTHES_UPDATE",HttpStatus.OK, "옷을 성공적으로 수정하였습니다!"),
-    CLOTHES_DELETE("CLOTHES_DELETE",HttpStatus.OK, "옷을 성공적으로 삭제하였습니다!");
+    CLOTHES_DELETE("CLOTHES_DELETE",HttpStatus.OK, "옷을 성공적으로 삭제하였습니다!"),
+    CLOTHES_IMAGE_REMOVE("CLOTHES_IMAGE_REMOVE",HttpStatus.OK, "옷에 등록된 이미지를 성공적으로 제거하였습니다!");
 
     private final String code;
     private final HttpStatus httpStatus;
