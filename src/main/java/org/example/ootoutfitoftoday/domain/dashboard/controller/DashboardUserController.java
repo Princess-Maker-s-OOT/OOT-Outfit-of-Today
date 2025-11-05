33df55796d954de16006f9f74ea55121ea6f1c9c
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-11-06 00:07:31 +0900

    [feat] dashboard: swagger 적용

--- src/main/java/org/example/ootoutfitoftoday/domain/dashboard/controller/DashboardUserController.java
+++ src/main/java/org/example/ootoutfitoftoday/domain/dashboard/controller/DashboardUserController.java
@@ -1,5 +1,7 @@
 package org.example.ootoutfitoftoday.domain.dashboard.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -22,12 +24,19 @@ import java.time.LocalDate;
 @SecurityRequirement(name = "bearerAuth")
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/v1/dashboards/users/overview")
+@RequestMapping("/v1/dashboards/users")
 public class DashboardUserController {
 
     private final DashboardUserQueryService dashboardUserQueryService;
 
-    @GetMapping
+    @Operation(
+            summary = "대시보드 옷 분포 현황 조회",
+            description = "사용자는 등록한 옷의 분포 현황을 조회합니다.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "조회 성공"),
+                    @ApiResponse(responseCode = "401", description = "인증 실패")
+            })
+    @GetMapping("/overview")
     public ResponseEntity<Response<DashboardUserSummaryResponse>> getUserDashboardSummary(
             @AuthenticationPrincipal AuthUser authUser
     ) {
@@ -35,6 +44,13 @@ public class DashboardUserController {
         return Response.success(dashboardUserQueryService.getUserDashboardSummary(authUser.getUserId()), DashboardSuccessCode.DASHBOARD_USER_SUMMARY_OK);
     }
 
+    @Operation(
+            summary = "대시보드 옷의 착용 횟수 및 기간 통계 정보 조회",
+            description = "사용자는 등록한 옷의 착용 횟수 및 기간 통계를 조회합니다.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "조회 성공"),
+                    @ApiResponse(responseCode = "401", description = "인증 실패")
+            })
     @GetMapping("/statistics")
     public ResponseEntity<Response<DashboardUserWearStatisticsResponse>> getUserWearStatistics(
             @AuthenticationPrincipal AuthUser authUser,
