2206bcffd4387a3832af1154d4512cb928d428bc
assokk <mijisuu@naver.com>
2025-11-06 12:17:18 +0900

    [chore] common: RestTemplate 설정 클래스 추가 (결제 API 연동용)

---
+++ src/main/java/org/example/ootoutfitoftoday/common/config/RestTemplateConfig.java
@@ -0,0 +1,19 @@
+package org.example.ootoutfitoftoday.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        factory.setConnectTimeout(3000); // 연결 타임아웃 (ms)
+        factory.setReadTimeout(5000);    // 읽기 타임아웃 (ms)
+
+        return new RestTemplate(factory);
+    }
+}
