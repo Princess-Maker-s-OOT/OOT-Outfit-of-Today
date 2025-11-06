a50902874202574ddd181cd00105713a45cbeebf
cjn404 <moi404@naver.com>
2025-11-06 10:13:57 +0900

    임시 테스트

--- src/main/java/org/example/ootoutfitoftoday/kakao/config/KakaoMapConfig.java
+++ src/main/java/org/example/ootoutfitoftoday/kakao/config/KakaoMapConfig.java
@@ -1,11 +1,6 @@
 package org.example.ootoutfitoftoday.kakao.config;
 
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
-
-import java.time.Duration;
 
 @Configuration
 public class KakaoMapConfig {
@@ -13,11 +8,11 @@ public class KakaoMapConfig {
     private static final int CONNECT_TIMEOUT = 5000; // 5초
     private static final int READ_TIMEOUT = 5000; // 5초
 
-    @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder
-                .setConnectTimeout(Duration.ofMillis(CONNECT_TIMEOUT))
-                .setReadTimeout(Duration.ofMillis(READ_TIMEOUT))
-                .build();
-    }
+//    @Bean
+//    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+//        return builder
+//                .setConnectTimeout(Duration.ofMillis(CONNECT_TIMEOUT))
+//                .setReadTimeout(Duration.ofMillis(READ_TIMEOUT))
+//                .build();
+//    }
 }
