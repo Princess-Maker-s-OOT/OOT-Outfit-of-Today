8ec48a2715c42bdc16876a894f9a0e3c39f3bb7b
assokk <mijisuu@naver.com>
2025-11-06 12:05:53 +0900

    [feat] Payment: 결제 승인 관련 에러/성공 코드 추가

---
+++ src/main/java/org/example/ootoutfitoftoday/domain/payment/exception/PaymentSuccessCode.java
@@ -0,0 +1,21 @@
+package org.example.ootoutfitoftoday.domain.payment.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.example.ootoutfitoftoday.common.exception.SuccessCode;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum PaymentSuccessCode implements SuccessCode {
+
+    PAYMENT_APPROVED(
+            "PAYMENT_APPROVED",
+            HttpStatus.OK,
+            "결제가 승인되었습니다."
+    );
+
+    private final String code;
+    private final HttpStatus httpStatus;
+    private final String message;
+}
