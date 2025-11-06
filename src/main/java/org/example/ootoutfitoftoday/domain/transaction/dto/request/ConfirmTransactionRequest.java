10e28d8812e12148d4cf7939cc1aea0c317115d1
assokk <mijisuu@naver.com>
2025-11-06 12:08:48 +0900

    [feat] Transaction: 결제 승인 요청 DTO 추가

---
+++ src/main/java/org/example/ootoutfitoftoday/domain/transaction/dto/request/ConfirmTransactionRequest.java
@@ -0,0 +1,13 @@
+package org.example.ootoutfitoftoday.domain.transaction.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ConfirmTransactionRequest {
+
+    @NotBlank(message = "결제 키는 필수입니다.")
+    private String paymentKey;
+}
