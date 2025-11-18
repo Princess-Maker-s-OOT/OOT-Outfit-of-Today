bb6dcdc32ca0c5f239c24ddbe7bb4e06a914542e
cjn404 <moi404@naver.com>
2025-11-19 02:56:43 +0900

    [refactor] all: CommonErrorCode에서 사용하지 않는 에러코드 삭제

--- src/main/java/org/example/ootoutfitoftoday/common/exception/CommonErrorCode.java
+++ src/main/java/org/example/ootoutfitoftoday/common/exception/CommonErrorCode.java
@@ -10,10 +10,8 @@ public enum CommonErrorCode implements ErrorCode {
 
     // ======= 현재 사용 중 ======
     UNEXPECTED_SERVER_ERROR("UNEXPECTED_SERVER_ERROR", HttpStatus.INTERNAL_SERVER_ERROR, "예상하지 못한 서버 오류가 발생했습니다."),
-    VALIDATION_ERROR("VALIDATION_ERROR", HttpStatus.BAD_REQUEST, "Validation 오류가 발생했습니다."),
+    VALIDATION_ERROR("VALIDATION_ERROR", HttpStatus.BAD_REQUEST, "Validation 오류가 발생했습니다.");
     // ========================
-
-    CACHE_IS_NULL("CACHE_IS_NULL", HttpStatus.INTERNAL_SERVER_ERROR, "해당 캐시는 존재하지 않습니다.");
 
     private final String code;
     private final HttpStatus httpStatus;
